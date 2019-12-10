2e41cec8637b64da1acb8334872f9199dc978d38
Jesse Krim <jesse.krim@gmail.com>
2019-12-09 19:45:21 -0500

    Fix for Deployment

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -18,9 +18,9 @@ class Search extends React.Component {
     // this.props.getSavedMovies(this.state.currentGenreId);
   }
 
-  getGenres() async() {
+  getGenres() {
     //make an axios request in this component to get the list of genres from your endpoint GET GENRES
-    await axios
+    axios
       .get('/movies/genres')
       .then(({ data }) => {
         this.setState({
